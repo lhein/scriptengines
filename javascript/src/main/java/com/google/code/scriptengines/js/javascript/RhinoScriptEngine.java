2690cdeb8fea03198876616d76c13b0b4d38cd04
lhein.smx <lhein.smx@a96ffc51-5a53-0410-9c8d-19a237e180b6>
2008-10-23 07:01:04 +0000

    put copyrights back in

--- javascript/src/main/java/com/google/code/scriptengines/js/javascript/RhinoScriptEngine.java
+++ javascript/src/main/java/com/google/code/scriptengines/js/javascript/RhinoScriptEngine.java
@@ -1,4 +1,28 @@
 /*
+ * Copyright (C) 2006 Sun Microsystems, Inc. All rights reserved. 
+ * Use is subject to license terms.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are 
+ * permitted provided that the following conditions are met: Redistributions of source code 
+ * must retain the above copyright notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list of 
+ * conditions and the following disclaimer in the documentation and/or other materials 
+ * provided with the distribution. Neither the name of the Sun Microsystems nor the names of 
+ * is contributors may be used to endorse or promote products derived from this software 
+ * without specific prior written permission. 
+
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY 
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER 
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR 
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON 
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
@@ -54,6 +78,23 @@ import org.mozilla.javascript.Wrapper;
 import com.google.code.scriptengines.js.util.ExtendedScriptException;
 import com.google.code.scriptengines.js.util.InterfaceImplementor;
 
+/**
+ * Implementation of <code>ScriptEngine</code> using the Mozilla Rhino
+ * interpreter.
+ *
+ * @author Mike Grogan
+ * @author A. Sundararajan
+ * @version 1.0
+ * @since 1.6
+ *
+ * Modified for phobos to remove some of the restrictions.
+ * Modified to allow subclassing and preprocessing of script source code.
+ * Modified to avoid using the RhinoTopLevel class, since that introduces
+ * a circularity that prevents objects from being garbage collected.
+ *
+ * @author Roberto Chinnici
+ *
+ */
 public class RhinoScriptEngine extends AbstractScriptEngine
         implements  Invocable, Compilable {
     
